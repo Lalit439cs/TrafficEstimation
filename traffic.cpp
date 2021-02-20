75978f92f2c31865a043ff0829da8a5d7b684c70
Arka Mandal <74552123+ArkaMandal2001@users.noreply.github.com>
2021-02-20 22:18:06 +0530

    Update traffic.cpp

--- traffic.cpp
+++ traffic.cpp
@@ -1,47 +1,55 @@
-//#include "stdafx.h"
-//cd /mnt/c/Users/User/Desktop/TrafficCV/
+//C:\Users\User\Desktop\TrafficCV
 //g++ topview.cpp -o test -pthread -std=c++11 `pkg-config --cflags --libs opencv`
 
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
 
 vector<Point2f> src,dst;
-int i=0;
-Mat topvemp, topvfull, emptypic, traffic;
-float w = 300, h = 600;//cropped image size
-string trafficpicloc = "res/traffic.jpg";//put image here
-string emptypicloc = "res/empty.jpg";//put image here
-bool imageshown=false;
+Mat topvemp, topvfull, emptypic, traffic,cropemptypic, croptrafficpic;
+string path;
 
-void mousePoints(int event, int x, int y, int flags, void* params) {
+void mousePointsO(int event, int x, int y, int flags, void* params) {
 	Mat* image = reinterpret_cast<Mat*>(params);
 	if (event == EVENT_LBUTTONDOWN) {
 		src.push_back(Point2f(x, y));
 	}
-	if ((src.size() == 4)&&(!imageshown)) {
-		//Point2f src[4] = { {949,275},{1282,264},{510,1044},{1505,1045} };
-		dst = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
+	if (src.size() == 4) {
+		dst = { {472.0f,52.0f},{472.0f,830.0f},{800.0f,830.0f},{800.0f,52.0f} };//values used by instructor
 		Mat wpmat = findHomography(src, dst);
-		warpPerspective(emptypic, topvemp, wpmat, Point(w, h));
-		warpPerspective(traffic, topvfull, wpmat, Point(w, h));
-		imshow("Empty", topvemp);
-		imshow("Traffic", topvfull);
-		imageshown = true;
+		warpPerspective(emptypic, topvemp, wpmat, emptypic.size());
+		warpPerspective(traffic, topvfull, wpmat, traffic.size());
+		cropemptypic = topvemp(Rect(10, 20, 100, 50));
+		croptrafficpic = topvfull(Rect(472, 52, 800 - 472, 830 - 52));
+		cropemptypic = topvemp(Rect(472,52,800-472,830-52));
+		imshow("Top Empty Image", topvemp);
+		imshow("Cropped Empty Image", cropemptypic);
+		imshow("Top Traffic Image", topvfull);
+		imshow("Cropped Traffic Image", croptrafficpic);
+		imwrite(path + "/croppedempty.jpg", cropemptypic);
+		imwrite(path + "/croppedtraffic.jpg", croptrafficpic);
+		imwrite(path + "/topempty.jpg", topvemp);
+		imwrite(path + "/toptraffic.jpg", topvfull);
+		src.clear();
 	}
 }
 
 int main() {
+	cout << "Enter directory path:\n";
+	cin >> path;
+	string trafficpicloc = path+"/traffic.jpg";//put image here
+	string emptypicloc = path + "/empty.jpg";//put image here
 	traffic = imread(trafficpicloc);
 	emptypic = imread(emptypicloc);
-	imshow("Original Image", emptypic);
-	setMouseCallback("Original Image", mousePoints, reinterpret_cast<void*>(&emptypic));
-	//imshow("Image", topvfull);
+	if ((!traffic.data)||(!emptypic.data)) {
+		cout << "Images not Found";
+		return 0;
+	}
+	imshow("Original Empty Image", emptypic);
+	imshow("Original Traffic Image", traffic);
+	setMouseCallback("Original Empty Image", mousePointsO, reinterpret_cast<void*>(&emptypic));
 	waitKey(0);
 	return 0;
 }
