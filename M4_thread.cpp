14f1370b1daecb0edff826608ac4cf2bbf1bc7fe
Lalit Ghunawat <59700225+Lalit439cs@users.noreply.github.com>
2021-03-28 11:17:54 +0530

    Update M4_thread.cpp

--- M4_thread.cpp
+++ M4_thread.cpp
@@ -11,22 +11,31 @@ using namespace std;
 
 vector<Point2f> src,dst;
 Mat emptypic ,wpmat;
-Mat vidframe, flow, prevframe,curframe;
+Mat vidframe, flow, prevframe;
 string empname,videoloc;
+string emptypicloc;
+int lkm;
 int movingcount,queuecount;
-int row_thread;
+int gap;
 int numthreads=4;
 
 void* subplayvideo( void* arg){
 	int *ith_ptr = (int*) arg;
-	int ith= *ith_ptr;
-	
-	int last= (ith+1) * row_thread;
+	int ith=lkm++;
+	cout<<ith<<endl;
+	int last= (ith+1) * gap;
+	int start= ith * gap;
 	if (ith==numthreads-1){last=vidframe.cols;}
 	
-	curframe=vidframe(Range::all(), Range(ith * row_thread ,last));
+	
+	
+	Mat curframe=vidframe(Range::all(), Range(start,last));
+	
+	
+	
 	if (prevframe.empty() == false) {
-			calcOpticalFlowFarneback(prevframe(Range::all(), Range(ith * row_thread ,last)), curframe, flow, 0.4, 1, 12, 2, 8, 1.2, 0);
+			Mat prevpart=prevframe(Range::all(), Range(start,last));
+			calcOpticalFlowFarneback(prevpart, curframe, flow, 0.4, 1, 12, 2, 8, 1.2, 0);
 			
 			for (int y = 0; y < curframe.rows; y += 5) {
 				for (int x = 0; x < curframe.cols; x += 5) {
@@ -43,41 +52,40 @@ void* subplayvideo( void* arg){
 			}
 		}pthread_exit(0);
 }
-void mousePointsO(int event, int x, int y, int flags, void* params) {
-	Mat* image = reinterpret_cast<Mat*>(params);
-	if (event == EVENT_LBUTTONDOWN) {
-		src.push_back(Point2f(x, y));
-	}
-	if (src.size() == 4) {
-		dst = { {472.0f,52.0f},{472.0f,830.0f},{800.0f,830.0f},{800.0f,52.0f} };//values used by instructor
-		wpmat = findHomography(src, dst);
-		src.clear();
-		
-	}
-}
+
 int main() {
 
-	cout << "Enter traffic image and video directory and name in separate lines:\n";
-	cin >> empname;
-	cin >> videoloc;
+	cout << "Enter empty image and video directory name in separate lines:"<<endl;
 	
 	
-	string emptypicloc =empname;//put image here
+	videoloc="/home/lkm/opencv_test/trafficvideo.mp4";
+	//cin >> empname;
+	//cin >> videoloc;
+	
+	emptypicloc ="/home/lkm/opencv_test/traffic.jpg";//put image here
+	
 	emptypic = imread(emptypicloc);
+	
 	cvtColor(emptypic, emptypic, COLOR_BGR2GRAY);
+	
 	if (!emptypic.data) {
-		cout << "Image not Found";
+		cout << "Image not Found"<<endl;
 		return 0;
 	}
 	imshow("Original Empty Image", emptypic);
-	setMouseCallback("Original Empty Image", mousePointsO, reinterpret_cast<void*>(&emptypic));
+	src = { {946.0f,260.0f},{523.0f,1066.0f},{1558.0f,1070.0f},{1294.0f,252.0f} };//hardcoded values for rapid testing
 	
-	
-	
+	dst = { {472.0f,52.0f},{472.0f,830.0f},{800.0f,830.0f},{800.0f,52.0f} };//values used by instructor
+		wpmat = findHomography(src, dst);
+			
 	VideoCapture cap(videoloc);
+	
 	//ofstream out("C:/Users/User/Desktop/TrafficCV/out.txt");
 	//out << "Frame Number" << "," << "Moving Density" << "," << "Queue Density" << "\n";
+	
+	
 	while (cap.read(vidframe)==true) {
+	
 		cvtColor(vidframe, vidframe, COLOR_BGR2GRAY);
 		absdiff(vidframe, emptypic, vidframe);
 		threshold(vidframe, vidframe, 50.0, 100.0, THRESH_BINARY);
@@ -85,34 +93,46 @@ int main() {
 		vidframe = vidframe(Rect(472, 52, 800 - 472, 830 - 52));
 		movingcount = 0;
 		queuecount = 0;
-		row_thread=vidframe.rows / numthreads;
+		gap=vidframe.cols / numthreads;
 		int framenum=cap.get(CAP_PROP_POS_FRAMES);
 		
 		// Launch thread
+		lkm=0;
+		
 		pthread_t tids[numthreads];
+		
 		for (int i = 0; i < numthreads; i++) {
-			
-
 			pthread_attr_t attr;
 			pthread_attr_init(&attr);
-			pthread_create(&tids[i], &attr, subplayvideo, &i);
+			//cout<<i<<endl;
+			int j=i;
+			//cout<<j<<endl;
+			pthread_create(&tids[i], &attr, subplayvideo, &j);
+			waitKey(300);
+			
+						
 	}
 		
 		// Wait until thread is done its work
 		for (int k = 0; k < numthreads; k++) {
 			pthread_join(tids[k], NULL);}
 		
-		cout << framenum<<","<< movingcount <<","<< queuecount << "\n";
+		cout << framenum<<","<< movingcount <<","<< queuecount <<endl;
 		//out << framenum << "," << movingcount << "," << queuecount << "\n";
 		imshow("Video", vidframe);
 		waitKey(30);
-		prevframe = vidframe;
-	}
+		prevframe = vidframe;}
+		destroyAllWindows();
+	
+	
+	
+	
+
 	//out.close();
 	
 	
 	
-	destroyAllWindows();
+	
 	waitKey(0);
 	return 0;
 }
